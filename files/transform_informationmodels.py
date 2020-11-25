0ab0547430f5594eef71bc0a0ac9a8613b3a252a
Hans Olav Sund <hans.olav.sund@digdir.no>
2020-11-25 15:48:57 +0100

    chore: testing part transform

--- files/transform_informationmodels.py
+++ files/transform_informationmodels.py
@@ -18,53 +18,51 @@ def transform(inputfile, inputfile_enh, inputfile_mongo):
     array_enh = info_models_enh["hits"]["hits"]
 
     print(len(array))
-    transformed = {"Checked": 0}
+    # transformed = {}
+    failed = {}
     for information_model in array:
         uri = information_model["_source"].get("harvestSourceUri")
         service_code_list = re.findall('schemas(\\d+)', uri)
         if len(service_code_list) == 0 or len(service_code_list) > 1:
-            transformed[uri] = "Regex failed"
+            failed[uri] = "Regex failed"
         else:
             service_code = service_code_list[0]
             mongo_data = mongo_ids.get(service_code)
             if mongo_data:
                 if len(mongo_data) > 1:
-                    transformed[service_code] = "Too many hits"
+                    failed[service_code] = "Too many hits"
                 elif len(mongo_data) == 0:
-                    transformed[service_code] = "Empty"
+                    failed[service_code] = "Empty"
             else:
-                transformed[service_code] = "None"
-        transformed["Checked"] = transformed["Checked"]+1
+                failed[service_code] = "None"
 
     for information_model in array_enh:
         uri = information_model["_source"].get("harvestSourceUri")
         service_code_list = re.findall('schemas(\\d+)', uri)
         if len(service_code_list) == 0 or len(service_code_list) > 1:
-            transformed[uri] = "Regex failed"
+            failed[uri] = "Regex failed"
         else:
             service_code = service_code_list[0]
             mongo_data = mongo_ids.get(service_code)
             if mongo_data:
                 if len(mongo_data) > 1:
-                    transformed[service_code] = "Too many hits"
+                    failed[service_code] = "Too many hits"
                 elif len(mongo_data) == 0:
-                    transformed[service_code] = "Empty"
+                    failed[service_code] = "Empty"
             else:
-                transformed[service_code] = "None"
-        transformed["Checked"] = transformed["Checked"]+1
-    return transformed
+                failed[service_code] = "None"
+    return failed
 
-
-        # if informationmodel["_source"].get("serviceType") != "Kontoopplysninger" and dataservice["_source"].get("apiSpecUrl"):
-        #     first = dataservice["_source"].get("harvest")["firstHarvested"]
-        #     dataservice2 = {"doc": {"id": dataservice["_id"],
-        #                             "harvest": {"firstHarvested": first,
-        #                                         "lastHarvested": dataservice["_source"].get("harvest")["lastHarvested"],
-        #                                         "changed": mapchanged(dataservice["_source"].get("harvest"), first)
-        #                                         }
-        #                             }
-        #                     }
-        #     transformed[dataservice["_source"].get("apiSpecUrl")] = dataservice2
+    # if informationmodel["_source"].get("serviceType") != "Kontoopplysninger" and dataservice["_source"].get("apiSpecUrl"):
+    #     first = dataservice["_source"].get("harvest")["firstHarvested"]
+    #     dataservice2 = {"doc": {"id": dataservice["_id"],
+    #                             "harvest": {"firstHarvested": first,
+    #                                         "lastHarvested": dataservice["_source"].get("harvest")["lastHarvested"],
+    #                                         "changed": mapchanged(dataservice["_source"].get("harvest"), first)
+    #                                         }
+    #                             }
+    #                     }
+    #     transformed[dataservice["_source"].get("apiSpecUrl")] = dataservice2
     # print("Total to be transformed: ", len(transformed))
     # return transformed
 
