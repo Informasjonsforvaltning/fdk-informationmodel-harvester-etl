4b487a2262543f1f0bb61de053197348e3baf950
Hans Olav Sund <hans.olav.sund@digdir.no>
2020-11-25 12:58:05 +0100

    chore: finished extract

--- files/extract_mongodata.py
+++ files/extract_mongodata.py
@@ -14,10 +14,10 @@ dict_list = list(db.informationmodel.find({}, {"_id": 1}))
 ids = {}
 for id_dict in dict_list:
     id_str = id_dict["_id"][2:-2]
-    id_str_mod = re.split('/', id_str)[-1]
-    id_str_mod_getting_there = re.split('-', id_str_mod)[0]
+    id_str_mod = id_str.rpartition('/')[2]
+    id_str_mod_getting_there = id_str_mod.partition('-')[0]
 
-    uriArray = ids[id_str_mod_getting_there]
+    uriArray = ids.get(id_str_mod_getting_there)
     uriArray = uriArray if uriArray else []
     uriArray.append(id_str)
     ids[id_str_mod_getting_there] = uriArray
