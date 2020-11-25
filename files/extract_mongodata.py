0219105c692db1a42433d6f8f366bae16e8faf0a
Hans Olav Sund <hans.olav.sund@digdir.no>
2020-11-25 10:41:08 +0100

    chore: finished extract

--- files/extract_mongodata.py
+++ files/extract_mongodata.py
@@ -10,7 +10,11 @@ parser.add_argument('-o', '--outputdirectory', help="the path to the directory o
 args = parser.parse_args()
 connection = MongoClient(f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/datasetCatalog?authSource=admin&authMechanism=SCRAM-SHA-1""")
 db = connection.informationModelHarvester
-ids = list(db.informationmodel.find({}, {"_id": 1}))
+dict_list = list(db.informationmodel.find({}, {"_id": 1}))
+ids = []
+for id_dict in dict_list:
+    id_str = id_dict["_id"]
+    ids.append(id_str)
 
 with open(args.outputdirectory + 'mongo_infmodels_id.json', 'w', encoding="utf-8") as outfile:
     json.dump(dumps(ids), outfile, ensure_ascii=False, indent=4)
