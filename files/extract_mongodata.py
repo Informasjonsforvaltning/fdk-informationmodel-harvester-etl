69a8797693c97f12ae2d0249fe2453717874d9c8
Hans Olav Sund <hans.olav.sund@digdir.no>
2020-11-26 13:13:32 +0100

    chore: testing part transform

--- files/extract_mongodata.py
+++ files/extract_mongodata.py
@@ -14,14 +14,16 @@ dict_list = list(db.informationmodel.find({}, {"_id": 1}))
 ids = {}
 for id_dict in dict_list:
     id_str = id_dict["_id"]
-    id_str_mod = id_str.rpartition('/')[2]
-    id_str_mod_getting_there = id_str_mod.partition('-')[0]
+    if "https://fdk-dev-altinn.appspot.com/api/v1/schemas" in id_str:
+        id_str_mod = id_str.rpartition('/')[2]
+        id_str_mod_getting_there = id_str_mod.partition('-')[0]
 
-    uriArray = ids.get(id_str_mod_getting_there)
-    uriArray = uriArray if uriArray else []
-    uriArray.append(id_str)
-    ids[id_str_mod_getting_there] = uriArray
-
+        uriArray = ids.get(id_str_mod_getting_there)
+        uriArray = uriArray if uriArray else []
+        uriArray.append(id_str)
+        ids[id_str_mod_getting_there] = uriArray
+    else:
+        ids[id_str] = id_str
 
 with open(args.outputdirectory + 'mongo_infmodels_id.json', 'w', encoding="utf-8") as outfile:
     json.dump(ids, outfile, ensure_ascii=False, indent=4)
--- files/transform_informationmodels.py
+++ files/transform_informationmodels.py
@@ -12,25 +12,32 @@ def transform(inputfile, inputfile_enh, inputfile_mongo):
     info_models = openfile(inputfile)
     info_models_enh = openfile(inputfile_enh)
     mongo_ids = openfile(inputfile_mongo)
-    print(mongo_ids)
+    models_map = openfile("models_map.json")
 
     array = info_models["hits"]["hits"]
     array_enh = info_models_enh["hits"]["hits"]
 
-    print(len(array))
+    print("Number of info_models:" + str(len(array)) + "\n")
+    print("Number of info_models_enh:" + str(len(array_enh)) + "\n")
     transformed = {}
     failed = {}
     for information_model in array:
         uri = information_model["_source"].get("harvestSourceUri")
+        identifier = information_model["_source"].get("identifier")
         service_code_list = re.findall('schemas(\\d+)', uri)
-        if len(service_code_list) == 0 or len(service_code_list) > 1:
-            failed[uri] = "Regex failed"
+        if mongo_ids.get(identifier):
+            transformed[identifier] = "Is OK (identifier)"
+        elif len(service_code_list) == 0 or len(service_code_list) > 1:
+            failed[uri] = "Not altinn-model"
         else:
             service_code = service_code_list[0]
             mongo_data = mongo_ids.get(service_code)
             if mongo_data:
                 if len(mongo_data) > 1:
-                    failed[service_code] = "Too many hits"
+                    if service_code in models_map:
+                        transformed[service_code] = "Is OK (models_map)"
+                    else:
+                        failed[service_code] = "Not found in models_map"
                 elif len(mongo_data) == 0:
                     failed[service_code] = "Empty"
                 else:
@@ -40,15 +47,21 @@ def transform(inputfile, inputfile_enh, inputfile_mongo):
 
     for information_model in array_enh:
         uri = information_model["_source"].get("harvestSourceUri")
+        identifier = information_model["_source"].get("identifier")
         service_code_list = re.findall('schemas(\\d+)', uri)
-        if len(service_code_list) == 0 or len(service_code_list) > 1:
-            failed[uri] = "Regex failed"
+        if mongo_ids.get(identifier):
+            transformed[identifier] = "Is OK (identifier)"
+        elif len(service_code_list) == 0 or len(service_code_list) > 1:
+            failed[uri] = "Not altinn-model"
         else:
             service_code = service_code_list[0]
             mongo_data = mongo_ids.get(service_code)
             if mongo_data:
                 if len(mongo_data) > 1:
-                    failed[service_code] = "Too many hits"
+                    if service_code in models_map:
+                        transformed[service_code] = "Is OK (models_map)"
+                    else:
+                        failed[service_code] = "Not found in models_map"
                 elif len(mongo_data) == 0:
                     failed[service_code] = "Empty"
                 else:
@@ -59,19 +72,6 @@ def transform(inputfile, inputfile_enh, inputfile_mongo):
     with open(failed_transform, 'w', encoding="utf-8") as failed_file:
         json.dump(failed, failed_file, ensure_ascii=False, indent=4)
     return transformed
-
-    # if informationmodel["_source"].get("serviceType") != "Kontoopplysninger" and dataservice["_source"].get("apiSpecUrl"):
-    #     first = dataservice["_source"].get("harvest")["firstHarvested"]
-    #     dataservice2 = {"doc": {"id": dataservice["_id"],
-    #                             "harvest": {"firstHarvested": first,
-    #                                         "lastHarvested": dataservice["_source"].get("harvest")["lastHarvested"],
-    #                                         "changed": mapchanged(dataservice["_source"].get("harvest"), first)
-    #                                         }
-    #                             }
-    #                     }
-    #     transformed[dataservice["_source"].get("apiSpecUrl")] = dataservice2
-    # print("Total to be transformed: ", len(transformed))
-    # return transformed
 
 
 def openfile(file_name):
