ffd6924e95fb79edd4779e6787e9319703809cbf
Hans Olav Sund <hans.olav.sund@digdir.no>
2020-11-30 11:46:39 +0100

    chore: minor rework

--- files/extract_mongodata.py
+++ files/extract_mongodata.py
@@ -7,7 +7,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
 args = parser.parse_args()
-connection = MongoClient(f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/datasetCatalog?authSource=admin&authMechanism=SCRAM-SHA-1""")
+connection = MongoClient(
+    f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/datasetCatalog?authSource=admin&authMechanism=SCRAM-SHA-1""")
 db = connection.informationModelHarvester
 dict_list = list(db.informationmodel.find({}, {"_id": 1}))
 ids = {}
--- files/load_informationmodels.py
+++ files/load_informationmodels.py
@@ -8,7 +8,8 @@ import bson
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
 args = parser.parse_args()
-connection = MongoClient(f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/datasetCatalog?authSource=admin&authMechanism=SCRAM-SHA-1""")
+connection = MongoClient(
+    f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/datasetCatalog?authSource=admin&authMechanism=SCRAM-SHA-1""")
 db = connection.informationModelHarvester
 
 with open(args.outputdirectory + 'informationmodels_transformed.json') as transformed_file:
