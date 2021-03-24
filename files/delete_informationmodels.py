9d063dea3eceafccfef8c1f38c05df74e5e404f2
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-03-24 15:11:01 +0100

    chore: ETL finished?

--- files/delete_informationmodels.py
+++ files/delete_informationmodels.py
@@ -1,27 +1,48 @@
 import json
-import os
-from pymongo import MongoClient
+import re
 import argparse
-import bson
+from pymongo import MongoClient
+import gridfs
+import os
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
 args = parser.parse_args()
+namespace = os.environ['NAMESPACE']
 connection = MongoClient(
-    f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/datasetCatalog?authSource=admin&authMechanism=SCRAM-SHA-1""")
+    f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/informationModelHarvester?authSource=admin&authMechanism=SCRAM-SHA-1""")
 db = connection.informationModelHarvester
 
-with open(args.outputdirectory + 'informationmodels_transformed.json') as transformed_file:
-    transformed_json = json.load(transformed_file)
-    for mongo_id in transformed_json:
-        print("Mongo_id: " + mongo_id)
-        values = transformed_json[mongo_id]
-        to_be_updated = {"fdkId": str(values["fdkId"]),
-                         "issued": bson.Int64(int(values["issued"])),
-                         "modified": bson.Int64(int(values["modified"]))}
-        print("To be updated: " + str(to_be_updated))
-        print(db.informationmodel.find_one_and_update({'_id': mongo_id},  {'$set': to_be_updated}))
-        # print(db.informationmodel.find_one_and_update({'_id': mongo_id},  {'$set': {"fdkId": str(values["fdkId"])}}))
-        # print(db.informationmodel.find_one_and_update({'_id': mongo_id},  {'$set': {"issued": bson.Int64(int(values["issued"]))}}))
-        # print(db.informationmodel.find_one_and_update({'_id': mongo_id},  {'$set': {"modified": bson.Int64(int(values["modified"]))}}))
+
+def transform(inputfile, infmodels):
+    filenames = openfile(inputfile)
+    inf_models = openfile(infmodels)
+    files = []
+    output = {}
+    for filename in filenames:
+        files.append(db.fs.files.find({"filename": f"{filename}"}, {"_id": 1}))
+
+    for gridfsid in files:
+        output[str(gridfsid) + "-chunks"] = db.fs.chunks.remove(gridfsid)
+        output[str(gridfsid) + "-files"] = db.fs.files.remove(gridfsid)
+
+    for models in inf_models:
+        model_id = inf_models[models]["_id"]
+        output[str(model_id) + "-model"] = db.informationModelsMeta.remove(model_id)
+    return output
+
+
+def openfile(file_name):
+    with open(file_name) as json_file:
+        return json.load(json_file)
+
+
+inputfileName = args.outputdirectory + "filenames.json"
+infModelsMeta = args.outputdirectory + "infmodelsMeta.json"
+outputfileName = args.outputdirectory + "deleted.json"
+
+
+# Transform the organization object to publisher format:
+with open(outputfileName, 'w', encoding="utf-8") as outfile:
+    json.dump(transform(inputfileName, infModelsMeta), outfile, ensure_ascii=False, indent=4)
