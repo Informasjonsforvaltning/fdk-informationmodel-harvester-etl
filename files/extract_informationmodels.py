fc268c80dae2fa4c7bc4df593449f359e8a17d1a
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-10-12 11:37:59 +0200

    chore: extract & transform finished

--- files/extract_informationmodels.py
+++ files/extract_informationmodels.py
@@ -11,33 +11,12 @@ connection = MongoClient(
     f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/informationModelHarvester?authSource=admin&authMechanism=SCRAM-SHA-1""")
 db = connection.informationModelHarvester
 
-namespace = os.environ['NAMESPACE']
-
-staging_catalogs = [{"_id": "https://www.altinn.no/models/altinn", "fdkId": "f27930d7-8070-3d12-b4c7-e6aae22a478f"},
-                    {"_id": "https://www.altinn.no/models/or", "fdkId": "73925a02-21a0-3ad9-948c-ac017299ec22"},
-                    {"_id": "https://www.altinn.no/models/seres", "fdkId": "c62b1bd8-17a1-3724-9d00-db020a2025b8"}]
-other_catalogs = [{"_id": "https://www.altinn.no/models/catalog", "fdkId": "30251da7-e78f-33e5-9249-375451c9b187"}]
-
-
-def get_part_of_uri(fdkId):
-    if namespace == "staging":
-        return f"https://informationmodels.staging.fellesdatakatalog.digdir.no/catalogs/{fdkId}"
-    elif namespace == "demo":
-        return f"https://informationmodels.demo.fellesdatakatalog.digdir.no/catalogs/{fdkId}"
-    else:
-        return f"https://informationmodels.fellesdatakatalog.digdir.no/catalogs/{fdkId}"
-
-
-if namespace == "staging":
-    catalogs = staging_catalogs
-else:
-    catalogs = other_catalogs
-
-infModelMeta_list = []
-for catalog in catalogs:
-    catalog_fdkId = catalog["fdkId"]
-    uri = get_part_of_uri(catalog_fdkId)
-    infModelMeta_list.extend(list(db.informationModelMeta.find({"isPartOf": f'{uri}'}, {"fdkId": 1})))
+dict_list = list(db.informationModelMeta.find(["isPartOf:/abd4d5cd-febe-3e08-ab97-35070fa3d7e9"]))
+inf_models = {}
+for id_dict in dict_list:
+    id_str = id_dict["_id"]
+    inf_models[id_str] = id_dict
+print("Total number of extracted informationmodels: " + str(len(inf_models)))
 
 with open(args.outputdirectory + 'infmodelsMeta.json', 'w', encoding="utf-8") as outfile:
-    json.dump(infModelMeta_list, outfile, ensure_ascii=False, indent=4)
+    json.dump(inf_models, outfile, ensure_ascii=False, indent=4)
---
+++ files/transform_informationmodels.py
@@ -0,0 +1,44 @@
+import json
+import argparse
+import os
+from hashlib import sha1
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
+args = parser.parse_args()
+namespace = os.environ['NAMESPACE']
+
+
+def transform(inputfile):
+    info_models = openfile(inputfile)
+    transformed_models = {}
+
+    for model_id in info_models:
+        new_id = create_hash(model_id)
+        for other_id in info_models:
+            if new_id in other_id:
+                transformed_models[other_id] = transform_model(info_models[model_id], info_models[other_id])
+    return transformed_models
+
+
+def transform_model(old_model, new_model):
+    new_model["fdkId"] = old_model["fdkId"]
+    new_model["issued"] = old_model["issued"]
+    return new_model
+
+
+def openfile(file_name):
+    with open(file_name) as json_file:
+        return json.load(json_file)
+
+
+def create_hash(old_id):
+    return sha1(bytes(old_id, encoding="utf-8")).hexdigest()
+
+
+inputfileName = args.outputdirectory + "infmodelsMeta.json"
+outputfileName = args.outputdirectory + "transformed_models.json"
+
+
+with open(outputfileName, 'w', encoding="utf-8") as outfile:
+    json.dump(transform(inputfileName), outfile, ensure_ascii=False, indent=4)
