2b3a5b0a277990527b22108651c5c3c4a9bb7e8f
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-10-12 15:11:04 +0200

    chore: ETL finished

--- files/load_informationmodels.py
+++ files/load_informationmodels.py
@@ -1,51 +1,18 @@
 import json
-import re
 import argparse
 from pymongo import MongoClient
-import gridfs
+
 import os
-import bson
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
 args = parser.parse_args()
-namespace = os.environ['NAMESPACE']
-staging_catalogs = [{"_id": "https://www.altinn.no/models/altinn", "fdkId": "f27930d7-8070-3d12-b4c7-e6aae22a478f"},
-                    {"_id": "https://www.altinn.no/models/or", "fdkId": "73925a02-21a0-3ad9-948c-ac017299ec22"},
-                    {"_id": "https://www.altinn.no/models/seres", "fdkId": "c62b1bd8-17a1-3724-9d00-db020a2025b8"}]
-other_catalogs = [{"_id": "https://www.altinn.no/models/catalog", "fdkId": "30251da7-e78f-33e5-9249-375451c9b187"}]
 connection = MongoClient(
     f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/informationModelHarvester?authSource=admin&authMechanism=SCRAM-SHA-1""")
 db = connection.informationModelHarvester
 
-if namespace == "staging":
-    catalogs = staging_catalogs
-else:
-    catalogs = other_catalogs
-
-
-def transform(inputfile, infmodels):
-    filenames = openfile(inputfile)
-    inf_models = openfile(infmodels)
-    files = []
-    output = []
-    for filename in filenames:
-        files.append(db.fs.files.find_one({"filename": f"{filename}"}, {"_id": 1}))
-
-    for gridfsid in files:
-        output.append(db.fs.chunks.delete_one({"files_id": gridfsid['_id']}).raw_result)
-        output.append(db.fs.files.delete_one({"_id": gridfsid['_id']}).raw_result)
-
-    for model in inf_models:
-        model_id = model["_id"]
-        output.append(db.informationModelMeta.delete_one({"_id": model_id}).raw_result)
-
-    for catalog in catalogs:
-        catalog_id = catalog["_id"]
-        output.append(db.catalogMeta.delete_one({"_id": catalog_id}).raw_result)
-
-    return output
+inputfileName = args.outputdirectory + "transformed_models.json"
 
 
 def openfile(file_name):
@@ -53,9 +20,23 @@ def openfile(file_name):
         return json.load(json_file)
 
 
-inputfileName = args.outputdirectory + "filenames.json"
-infModelsMeta = args.outputdirectory + "infmodelsMeta.json"
-outputfileName = args.outputdirectory + "deleted.json"
+transformed_json = openfile(inputfileName)
 
-with open(outputfileName, 'w', encoding="utf-8") as outfile:
-    json.dump(transform(inputfileName, infModelsMeta), outfile, ensure_ascii=False, indent=4)
+total_updated = 0
+total_failed = 0
+fail_log = {}
+for mongo_id in transformed_json:
+    to_be_updated = transformed_json[mongo_id]
+    print("Updating ID: " + mongo_id)
+    update_result = db.informationModelMeta.find_one_and_update(to_be_updated)
+    if update_result:
+        total_updated += 1
+        print("Successfully updated: " + mongo_id)
+    else:
+        total_failed += 1
+        print("Update failed: " + mongo_id)
+        fail_log[mongo_id] = mongo_id
+print("Total number of records updated: " + str(total_updated))
+print("Total number of record updates failed: " + str(total_failed))
+with open(args.outputdirectory + "load_errors.json", 'w', encoding="utf-8") as err_file:
+    json.dump(fail_log, err_file, ensure_ascii=False, indent=4)
