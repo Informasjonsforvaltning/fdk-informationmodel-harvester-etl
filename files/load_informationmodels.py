5a4802bdfb78049440533274c6374c78a4910128
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-10-14 09:26:23 +0200

    chore: load+delete finished

--- files/load_informationmodels.py
+++ files/load_informationmodels.py
@@ -12,7 +12,8 @@ connection = MongoClient(
     f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/informationModelHarvester?authSource=admin&authMechanism=SCRAM-SHA-1""")
 db = connection.informationModelHarvester
 
-inputfileName = args.outputdirectory + "transformed_models.json"
+input_fileName = args.outputdirectory + "transformed_models.json"
+delete_fileName = args.outputdirectory + "old_models.json"
 
 
 def openfile(file_name):
@@ -20,7 +21,9 @@ def openfile(file_name):
         return json.load(json_file)
 
 
-transformed_json = openfile(inputfileName)
+transformed_json = openfile(input_fileName)
+delete_json = openfile(delete_fileName)
+
 
 total_updated = 0
 total_failed = 0
@@ -39,4 +42,23 @@ for mongo_id in transformed_json:
 print("Total number of records updated: " + str(total_updated))
 print("Total number of record updates failed: " + str(total_failed))
 with open(args.outputdirectory + "load_errors.json", 'w', encoding="utf-8") as err_file:
+    json.dump(fail_log, err_file, ensure_ascii=False, indent=4)
+
+
+total_deleted = 0
+total_failed = 0
+fail_log = {}
+for mongo_id in delete_json:
+    print("Deleting ID: " + mongo_id)
+    delete_result = db.informationModelMeta.delete_one({"_id": mongo_id})
+    if delete_result["acknowledged"]:
+        total_deleted += 1
+        print("Successfully deleted: " + mongo_id)
+    else:
+        total_failed += 1
+        print("Delete failed: " + mongo_id)
+        fail_log[mongo_id] = mongo_id
+print("Total number of records updated: " + str(total_updated))
+print("Total number of record updates failed: " + str(total_failed))
+with open(args.outputdirectory + "delete_errors.json", 'w', encoding="utf-8") as err_file:
     json.dump(fail_log, err_file, ensure_ascii=False, indent=4)
--- files/transform_informationmodels.py
+++ files/transform_informationmodels.py
@@ -6,17 +6,24 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
 args = parser.parse_args()
 
+inputfileName = args.outputdirectory + "infmodelsMeta.json"
+outputfileName = args.outputdirectory + "transformed_models.json"
+old_models_filename = args.outputdirectory + "old_models.json"
+
 
 def transform(inputfile):
     info_models = openfile(inputfile)
     transformed_models = {}
-
+    old_models = {}
     for model_id in info_models:
         new_id = create_hash(model_id)
         print("New id: " + new_id)
         for other_id in info_models:
             if new_id in other_id:
                 transformed_models[other_id] = transform_model(info_models[model_id], info_models[other_id])
+                old_models[model_id] = info_models[model_id]
+    with open(old_models_filename, 'w', encoding="utf-8") as old_models_file:
+        json.dump(old_models, old_models_file, ensure_ascii=False, indent=4)
     return transformed_models
 
 
@@ -34,10 +41,6 @@ def openfile(file_name):
 def create_hash(old_id):
     print("Old id: " + old_id)
     return sha1(bytes(old_id, encoding="utf-8")).hexdigest()
-
-
-inputfileName = args.outputdirectory + "infmodelsMeta.json"
-outputfileName = args.outputdirectory + "transformed_models.json"
 
 
 with open(outputfileName, 'w', encoding="utf-8") as outfile:
